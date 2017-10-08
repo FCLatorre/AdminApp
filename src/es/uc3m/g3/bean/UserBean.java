16d1655129d7d6bb2b41b0fc67f5ad8aaa827b8f
Tecnologias de Internet para la web <tiw@vagrant.vm>
2017-10-08 14:03:28 +0000

    Create simple user bean

---
+++ src/es/uc3m/g3/bean/UserBean.java
@@ -0,0 +1,28 @@
+package es.uc3m.g3.bean;
+
+public class UserBean {
+	private String email;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public UserBean() {
+		super();
+	}
+	public UserBean(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+}
