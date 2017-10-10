ba5df3d13622c3a844578f91452ced7c41864b58
carlososm <carlos.olivares.sm@gmail.com>
2017-10-10 09:17:01 +0200

    EventBean (without UserId, image as String and Location as String)

---
+++ src/es/uc3m/g3/bean/EventBean.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+
+package es.uc3m.g3.bean;
+
+public class EventBean {
+  private String title;
+  private String description;
+  private Date date;
+  private String location;
+  private String hall;
+  private String image;
+  private short tickets;
+  private double price;
+  private String category;
+
+  public String getTitle() { return title; }
+  public String getDescription() { return description; }
+  public Date getDate() { return date; }
+  public String getLocation() { return location; }
+  public String getHall() { return hall; }
+  public String getImage() { return image; }
+  public short getTickets() { return tickets; }
+  public double getPrice() { return price; }
+  public String getCategory() { return category; }
+
+  public void setTitle(String title) { this.title = title; }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public void setDate(Date date) { this.date = date; }
+  public void setLocation(String location) { this.location = location; }
+  public void setHall(String hall) { this.hall = hall; }
+  public void setImage(String image) { this.image = image; }
+  public void setTickets(short tickets) { this.tickets = tickets; }
+  public void setPrice(double price) { this.price = price; }
+  public void setCategory(String category) { this.category = category; }
+
+  public EventBean() { super(); }
+  public EventBean(String title, String description, Date date, String location,
+                   String hall, String image, short tickets, double price,
+                   String category) {
+    super();
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.location = location;
+    this.hall = hall;
+    this.image = image;
+    this.tickets = tickets;
+    this.price = price;
+    this.category = category;
+  }
+}
