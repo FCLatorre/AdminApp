7777d20686dee6b9e4fed5b8940d1a79a569bdc6
carlososm <carlos.olivares.sm@gmail.com>
2017-10-15 11:31:28 +0200

    return true to simulate user is always registrated

--- src/es/uc3m/g3/handlers/RestrictedRequestHandler.java
+++ src/es/uc3m/g3/handlers/RestrictedRequestHandler.java
@@ -8,7 +8,8 @@ import es.uc3m.g3.bean.UserBean;
 public class RestrictedRequestHandler {
 	protected boolean checkRegisteredUser (HttpServletRequest request, UserBean user) {		
 		HttpSession session = request.getSession();
-		
-		return session.getAttribute("user") != null;
+
+		//return session.getAttribute("user") != null;
+		return true;
 	}
 }
