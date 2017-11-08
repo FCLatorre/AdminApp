c39e35acc9c80d8af2ee144cd34bfde929875d6e
FCLatorre <fernandoconde29@gmail.com>
2017-11-08 17:05:58 +0000

    Add the necessary entities to work

---
+++ src/es/uc3m/g3/entities/EntidadAdministrador.java
@@ -0,0 +1,59 @@
+package es.uc3m.g3.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the EntidadAdministrador database table.
+ * 
+ */
+@Entity
+@Table(name="EntidadAdministrador")
+@NamedQuery(name="EntidadAdministrador.findAll", query="SELECT e FROM EntidadAdministrador e")
+public class EntidadAdministrador implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="Id")
+	private String id;
+
+	//bi-directional many-to-one association to Conversacion
+	@OneToMany(mappedBy="entidadAdministrador")
+	private List<Conversacion> conversacions;
+
+	public EntidadAdministrador() {
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<Conversacion> getConversacions() {
+		return this.conversacions;
+	}
+
+	public void setConversacions(List<Conversacion> conversacions) {
+		this.conversacions = conversacions;
+	}
+
+	public Conversacion addConversacion(Conversacion conversacion) {
+		getConversacions().add(conversacion);
+		conversacion.setEntidadAdministrador(this);
+
+		return conversacion;
+	}
+
+	public Conversacion removeConversacion(Conversacion conversacion) {
+		getConversacions().remove(conversacion);
+		conversacion.setEntidadAdministrador(null);
+
+		return conversacion;
+	}
+
+}
